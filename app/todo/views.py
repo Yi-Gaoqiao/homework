1f6e9d90a16bea645b34071c8568fb0f21c22ddc
Tsubasa Takahashi <2bsttas@gmail.com>
2020-07-12 13:52:02 +0900

    Added create tag feature.

--- app/todo/views.py
+++ app/todo/views.py
@@ -7,9 +7,19 @@ from core.models import Todo
 from todo import serializers
 
 
-class TodoViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage Todo in the database"""
+class TodoViewSet(viewsets.GenericViewSet, 
+                  mixins.ListModelMixin,
+                  mixins.CreateModelMixin):
+    """Manage Todo item in the database"""
     authentication_classes = (TokenAuthentication,)
     permissions_classes = (IsAuthenticated,)
     serializer_class = serializers.TodoSerializer
     queryset = Todo.objects.order_by('-created_at')
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('created_at')
+    
+    def perform_create(self, serializer):
+        """Create a new Todo"""
+        serializer.save(user=self.request.user)
