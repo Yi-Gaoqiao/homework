c80ae9434bd5046d216966d29979a4af2e83234a
Tsubasa Takahashi <2bsttas@gmail.com>
2020-07-12 16:49:57 +0900

    Modified settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6xegj9*r()%vh1frro0sms25sz*r4joh&z!s(&iv1&3zw)t9$o'
+
+try:
+    from . import local_settings
+except ImportError:
+    pass
+
+SECRET_KEY = local_settings.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
